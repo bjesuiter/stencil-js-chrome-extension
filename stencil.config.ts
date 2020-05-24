1d990aaf990cabd4de261a6ce76bd98e1e6b9393
Benjamin Jesuiter <benjamin.jesuiter@flavia-it.de>
2020-05-24 18:20:05 +0200

    make app-root work in chrome extension

--- stencil.config.ts
+++ stencil.config.ts
@@ -11,7 +11,11 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/'
+      baseUrl: 'https://myapp.local/',
+      prerenderConfig: './stencil.prerender.config.ts',
     }
-  ]
+  ],
+  // this disables all inlining by the stencil compiler
+  // => needed for chrome extension because inline script is viewed as evil in chrome
+  allowInlineScripts: false
 };
